1d551bda5a34c5564d517dc772f2c587f84235bc
Luca Bernstein <luca.bernstein@sap.com>
2026-01-08 13:26:57 +0100

    Improve startup entrypoint script
    Co-authored-by: Martin Weindel <martin.weindel@sap.com>

--- hack/git-http-backend/entrypoint.sh
+++ hack/git-http-backend/entrypoint.sh
@@ -6,36 +6,36 @@
 
 set -e
 
-if [ ! -f /data/gitea/conf/app.ini ]; then
-  cp /app.ini.sample /data/gitea/conf/app.ini
+if [ ! -d /data/gitea/conf ]; then
+   mkdir -p /data/gitea/conf
 fi
 
-# Start Forgejo in the background for initial setup
-/usr/bin/entrypoint &
-FG_PID=%1
+if [ ! -f /data/gitea/conf/app.ini ]; then
+   cp /app.ini.sample /data/gitea/conf/app.ini
+fi
 
-# Wait until the server is ready
-until curl -sf http://localhost:3000/ >/dev/null 2>&1; do
-  echo "Waiting for Forgejo..."
-  sleep 2
-done
+create_user_and_repo() {
+  # Wait until the server is ready
+  until curl -sf http://localhost:3000/ >/dev/null 2>&1; do
+    echo "Waiting for Forgejo..."
+    sleep 2
+  done
 
-# Create default admin user (replace values if needed)
-su - git -c "/usr/local/bin/forgejo admin user create \
-  --username test \
-  --password testtest \
-  --email test@example.com \
-  --admin || true"
+  # Create default admin user (replace values if needed)
+  su - git -c "/usr/local/bin/forgejo admin user create \
+    --username test \
+    --password testtest \
+    --email test@example.com \
+    --admin || true"
 
-# Create default repository
-curl -H "Content-Type: application/json" \
-  -d '{"name":"repo"}' \
-  -u test:testtest \
-  -X POST \
-  http://localhost:3000/api/v1/user/repos
+  # Create default repository
+  curl -H "Content-Type: application/json" \
+    -d '{"name":"repo"}' \
+    -u test:testtest \
+    -X POST \
+    http://localhost:3000/api/v1/user/repos
+}
 
-# Kill the background server and run in foreground
-kill $FG_PID
-wait $FG_PID
+create_user_and_repo &
 
-exec /usr/bin/entrypoint
+/usr/bin/entrypoint
